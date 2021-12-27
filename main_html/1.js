448c9c65a19d1f8a76bbd87cec812ea0a783c4e2
cats0713 <cats0713@naver.com>
2021-12-27 23:02:41 +0900

    study

---
+++ main_html/1.js
@@ -0,0 +1,10 @@
+(function main(){
+  function sum_num(inA, inB){
+	let temp_v = inA +inB;
+	return temp_v;
+  }
+  let input_A = 100;
+  let input_B = 200;
+  let to_tn = sum_num(input_A, input_B);
+  console.log(to_tn);
+})();

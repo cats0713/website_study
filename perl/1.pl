68b91cbcc42adc14acbc864d0dbd08ff091261dc
cats0713 <cats0713@naver.com>
2022-01-19 14:09:27 +0900

    공부

---
+++ perl/1.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl
+# 위에는 경로
+#hello perl  파일이름
+
+#print "Hello World\n";
+
+
+#$A = 'dd';
+#$B = $A*2;
+#print "$B";
+
+
+@userarr = (0,1,'i can do anyting');
+#$userarr[0] = 0;
+#$userarr[1] = 1;
+#$userarr[2] = "i can do anyting";
+
+print "$userarr[0]\n";
+print $userarr[2],"\n"
+#print "$#userarr";
+
+#$lastIndex = $#userarr;
+
+#print "$lastIndex";
+
+%myhash = ( "apple", 3, "pear", 10, "banana", 4, "monkey", 2 );
+$howmany = $myhash{ "banana" }
+print "$howmany\n";
+
---
+++ perl/2.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/perl
+
+%myhash = ("a",1 ,"b",2, "c",3);
+@userarr = (1,2,3,4,5);
+
+$userarr[9] = 9;
+print $userarr,"\n";
+
+# $mycont = 3;
+# print $mycont,"\n";
+
+# print @userarr;
+# #$howmany = $myhash{"c"};
+# #print "$howmany\n"
+
+# #$howmany = $myhash{"a"};
+
+# $myhash{"a"} = 9;
+# print $myhash{"a"},"\n";
+# print %myhash,"\n";
+
+# print <<end_of_hell;
+# Hello world.\n
+# I am here to serve you but not to conquer.
+# There were many kings and queens in the world,
+# but I want to be a real friend of yours.dsds
+# end_of_hell
+
+# print "Ending our greeting\n";
+

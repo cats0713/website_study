0e8240f99c2e1f57e855af07b80dd1680c6a83aa
cats0713 <cats0713@naver.com>
2022-02-08 16:27:15 +0900

    class  공부

--- js/class.js
+++ js/class.js
@@ -18,5 +18,57 @@ window.onload = () => {
   you.sayHello('hoho ');
   console.log(you.age);
 
+  const Foo = class Myclass{};
+  console.log(Foo);
+  // console.log(Myclass);
+  const foo = new Foo();
+  //new Myclass();
+
+  class food{
+    // constructor(){}
+    hi(a){
+      console.log(a);
+    }
+  }
+  const me = new food();
+  me.hi("add");
+  console.log(me);
+
+  const me2 = new food();
+  me2.hi();
+
+  class userA{
+    constructor(arrA = []){
+      this.arrA = arrA;
+
+    }
+    get firstElem(){
+      return this.arrA.length ? this.arrA[0] : null; 
+    }
+
+    set firstEle(elem){
+      this.arrA = [elem, ...this.arrA];
+    }
+  } //class filed에 접근하는 것을 방지 하기위해
+
+  const userB = new userA([1,2,3,4]);
+
+  console.log(userB.firstElem);
+
+  userB.firstEle = 100; //set 표기만 가능하다.
+
+  console.log(userB.firstElem);
+
+
+
+
+
+
+
+
+
+
+
+
 }
 

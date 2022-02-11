4c8801593857f03bf426b2b04ed8f6e8f5eb6bf0
cats0713 <cats0713@naver.com>
2022-02-11 09:41:08 +0900

    php 공부

---
+++ js/js_arr.js
@@ -0,0 +1,18 @@
+window.onload=()=>{
+
+  let str = "hello world";
+
+  console.log(str.charAt(4)); //index번호에 해당하는 문자를 반환
+  console.log(str.indexOf("l")); //문자열 왼쪽에서 부터 문자를 찾아 첫번재 값이맞는 문자의 인덱스 값을 반환
+  console.log(str.lastIndexOf("l")); //문자열 오른쪽에서 부터 문자를 찾아 첫번재 값이맞는 문자의 인덱스 값을 반환
+  console.log(str.match("ll"));//문자열 왼쪽부터 문자열을 맞는 단어 반환
+  console.log(str.replace("w","l"));//
+  console.log(str.search("s"));
+  console.log(str.lastIndexOf(""));//
+  console.log(str.lastIndexOf(""));//
+
+
+
+
+
+}
---
+++ php/2.php
@@ -0,0 +1,9 @@
+<?
+  // $inID = $_GET['id'];
+  // $inAGE = $_GET['age'];
+  $inID = $_POST['id'];
+  $inAGE = $_POST['age'];
+  echo 'name '.$inID.' age '.$inAGE;
+
+
+?>

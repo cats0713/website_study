90c91346f8ad47983a9f32940f0385095385c801
cats0713 <cats0713@naver.com>
2022-02-15 17:25:44 +0900

    php,sql,html  sql에 값 저장하기

--- sqltest/main.php
+++ sqltest/main.php
@@ -21,35 +21,18 @@
     include "./sub/nav.html";
     // nav.html 포함시키는 라인 
 
-    if(!@$_GET['menu']){ //get방식으로 가져왔을때 값이 메뉴면
-      echo '<script>location.href="./main.php?menu=allList";</script>';
+    if(!@$_GET['menu']){ //get방식으로 가져왔을때 값이 메뉴가 아니면
+      echo '<script>location.href="./main.php?menu=allList";</script>'; //allList로 이식
     }else if($_GET['menu'] == "allList"){ //전체 목록을 클릭했을 때
 
-      require "./include/db_info.inc"; //require 없으면 로딩이 안된다.
+      require "./include/db_open.inc"; //require 없으면 로딩이 안된다.
       //보안상 파일을 나눠서 한다 / php인지 inc 인지 구분이 안가니까 저렇게 inc로 바꾼다.
-
-      $db_conn = mysqli_connect($db_server, $db_id, $db_pw, $db_db); //db 접속
-      //db 핸들러
-
-      //핸들러 작업이 정상적으로 되지 않았으면 error
-      if(!$db_conn){ 
-        //echo "mysql_error($db_conn)";
-        echo "<script>alert('db 진입 실패');</script>"; 
-        exit;
-      }
-
-      //db 접속이 제대로 되지 않았으면 error
-      if(!@mysqli_select_db($db_conn,$db_db)){ 
-        //echo "mysql_error($db_conn)";
-        echo "<script>alert('db 선택 실패');</script>"; 
-        exit;
-      }
 
       // 변수에 쿼리 명령문 저장
       $myQuery = "select * from `telList`"; 
 
       //쿼리가 제대로 실행되지 않았을 경우
-      if(!$queryResult = mysqli_query($db_conn,$myQuery)){
+      if(!$queryResult = mysqli_query($db_conn, $myQuery)){
         echo "<script>alert('db query 실패');</script>";
         exit;
       }
@@ -60,17 +43,41 @@
         exit;
       }
 
+      $recCounter = 1;
       while ($current_row = mysqli_fetch_assoc($queryResult)){
         echo "<div>";
-        echo "<span>".$current_row['no']."</span>";
+        echo "<span>".$recCounter."</span>";
         echo "<span>".$current_row['name']."</span>";
         echo "<span>".$current_row['mobile']."</span>";
         echo "<span>".$current_row['address']."</span>";
+        echo "<span>[삭제]</span>";
         echo "</div>";
+        $recCounter++;
       }
       mysqli_free_result($queryResult);
       mysqli_close($db_conn);
+    }else if($_GET['menu'] == "addList"){ //목록 추가를 클릭했을 때
+      echo '<form action="./main.php" method="get">';
+      echo '<input type = "hidden" name = "menu" value="saveList">';
+      echo '<input type = "text" name = "name" placeholder = "이름" tabindex=10 autofocus required>';
+      echo '<input type = "text" name = "mobile" placeholder = "전화번호" tabindex=10 required>';
+      echo '<input type = "text" name = "address" placeholder = "주소" tabindex=10>';
+      echo '<input type = "submit" value="저장">'; 
+      echo '</form>';
+    }else if($_GET['menu'] == "saveList"){ //저장버튼을 눌렀을때
+      echo "save"; //save를 출력
+
+      $inName = $_GET['name']; //받아오기
+      $inMobile = $_GET['mobile'];
+      $inAddress = $_GET['address'];
+
+      $myQuery = "insert into `telList` values ('', '".$inName."','".$inMobile."','".$inAddress."')"; //쿼리 명령문
+      require "./include/db_open.inc"; //db_open
+      mysqli_query($db_conn,$myQuery);
+      mysqli_close($db_conn);
+      echo '<script>location.href="./main.php";</script>';
     }
+
   ?>
 </body>
 </html>

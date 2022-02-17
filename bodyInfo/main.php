2e0b1c8d36111398a6505e3b473e0e83b2498bd8
cats0713 <cats0713@naver.com>
2022-02-17 16:12:37 +0900

    php

--- bodyInfo/main.php
+++ bodyInfo/main.php
@@ -5,16 +5,18 @@
   require "./func/db_connect.php";
   $db_handle = DB_CONNECTFUNC($server, $db_id, $db_pw, $db_name);
 
-  $sqlQuery = "select * from `bodyInfo`";
-  $rows = mysqli_query($db_handle,$sqlQuery);
-  if(mysqli_num_rows($rows)==0){
+  $sqlQuery = "select * from `bodyInfo`"; //쿼리 명령문
+  $rows = mysqli_query($db_handle,$sqlQuery); //쿼리 날리기
+  if(mysqli_num_rows($rows)==0){ //자료없음
     echo "자료없음";
     exit;
   }
-  while($row = mysqli_fetch_assoc($rows)){
-    echo $row['no']," ".$row['name']."".$row['gender']." ".$row['height']." ".$row['weight']." ".$row['blood']."<br>";
+  while($row = mysqli_fetch_assoc($rows)){ //반복하여 문서에 기술
+    echo $row['no']," ".$row['name']." ".$row['gender']." ".$row['height']." ".$row['weight']." ".$row['blood'];
+    echo "<a href="./main.php"></a><br>";
   }
-
+  mysqli_free_result($rows);
+  mysqli_close($db_handle);
 
   require "./sub/footer.html";
 ?>

3ae25376908e54093d8253e817b06ac6dee2c3b4
cats0713 <cats0713@naver.com>
2022-02-17 17:18:14 +0900

    php  사이트

--- bodyInfo/main.php
+++ bodyInfo/main.php
@@ -3,31 +3,66 @@
 
   require "./inc/db_info.inc";
   require "./func/db_connect.php";
-  $db_handle = DB_CONNECTFUNC($server, $db_id, $db_pw, $db_name);
+  
 
-  $sqlQuery = "select * from `bodyInfo`"; //쿼리 명령문
-  $rows = mysqli_query($db_handle,$sqlQuery); //쿼리 날리기
-  if(mysqli_num_rows($rows)==0){ //자료없음
-    echo "자료없음";
-    exit;
-  }
-  echo "<section class='dataList'>";
-  echo "<article>";
-  echo "<ol>";
-  while($row = mysqli_fetch_assoc($rows)){ //반복하여 문서에 기술
-    echo "<li>".$row['name'];
-    echo $row['gender'];
-    echo $row['height'];
-    echo $row['weight'];
-    echo $row['blood']."</li>";
-    //echo "<a href='./main.php?menu=updateList&item=".$row['no']."'>[수정]</a><br>";
-    
-  }
-  echo "</ol>";
-  echo "</article>";
-  echo "</section>";
-  mysqli_free_result($rows);
-  mysqli_close($db_handle);
+  $menu = @$_GET['menu'];
+  if($menu == 'update'){
+    $db_handle = DB_CONNECTFUNC($server, $db_id, $db_pw, $db_name);
 
-  require "./sub/footer.html";
+    $sqlQuery = "select * from `bodyInfo` where no=".$_GET['no']""; //쿼리 명령문
+    $rows = mysqli_query($db_handle,$sqlQuery); //쿼리 날리기
+    if(mysqli_num_rows($rows)==0){ //자료없음
+      echo "자료없음";
+      exit;
+    }
+
+
+    echo "<section class='dataList'>";
+    echo "<article>";
+    echo "<ol>";
+    while($row = mysqli_fetch_assoc($rows)){ //반복하여 문서에 기술
+      echo "<li>".$row['name']." ";
+      echo $row['gender']." ";
+      echo $row['height']." ";
+      echo $row['weight']." ";
+      echo $row['blood']." ";
+      echo "<a href='main.php?menu=update&no=".$row['no']."'>수정</a>.</li><br>";
+    }
+    echo "</ol>";
+    echo "</article>";
+    echo "</section>";
+    mysqli_free_result($rows);
+    mysqli_close($db_handle);
+  
+    require "./sub/footer.html";
+  }eles{
+    $db_handle = DB_CONNECTFUNC($server, $db_id, $db_pw, $db_name);
+
+    $sqlQuery = "select * from `bodyInfo`"; //쿼리 명령문
+    $rows = mysqli_query($db_handle,$sqlQuery); //쿼리 날리기
+    if(mysqli_num_rows($rows)==0){ //자료없음
+      echo "자료없음";
+      exit;
+    }
+
+    echo "<section class='dataList'>";
+    echo "<article>";
+    echo "<ol>";
+    while($row = mysqli_fetch_assoc($rows)){ //반복하여 문서에 기술
+      echo "<li>".$row['name']." ";
+      echo $row['gender']." ";
+      echo $row['height']." ";
+      echo $row['weight']." ";
+      echo $row['blood']." ";
+      echo "<a href='main.php?menu=update&no=".$row['no']."'>수정</a>.</li><br>";
+    }
+    echo "</ol>";
+    echo "</article>";
+    echo "</section>";
+    mysqli_free_result($rows);
+    mysqli_close($db_handle);
+  
+    require "./sub/footer.html";
+  }
+
 ?>

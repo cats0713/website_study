fe548b5b9a76059bba1ca83019c7b3f1a56ac971
cats0713 <cats0713@naver.com>
2022-02-18 12:17:50 +0900

    php 공부

--- note/page/listnote.php
+++ note/page/listnote.php
@@ -1,7 +1,29 @@
 <?
   $search = @$_GET['search'];
   if($search){
-    echo $search;
+    require "../inc/db_info.inc";
+    require "../prg/db_connect.php";
+
+    $db_query = "select * from `note` where `name`='".$search."'";
+    $records = mysqli_query($db_handler,$db_query);
+    
+    if(mysqli_num_rows($records) == 0){
+      echo "<p>";
+      echo "<span>자료 없음</span>";
+      echo "</p>"; 
+    }else{
+      while($row = mysqli_fetch_assoc($records)){
+        echo "<p>";
+        echo "<span>".$row['name']."</span>";
+        echo "<span>".$row['apple']."</span>";
+        echo "<span>".$row['samsung']."</span>";
+        echo "<span>".$row['lg']."</span>";
+        echo "</p>";
+      }
+      mysqli_free_result($records);
+      mysqli_close($db_handler); 
+    }
+
   }else{
     require "../inc/db_info.inc";
     require "../prg/db_connect.php";

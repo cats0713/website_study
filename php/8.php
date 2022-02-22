66e954454b47b8e7e35d034c9c7674417027dc2a
cats0713 <cats0713@naver.com>
2022-02-22 14:27:15 +0900

    php  공부

--- php/8.php
+++ php/8.php
@@ -8,4 +8,32 @@
   unset($value);
   var_export($arr);
 
+  $a = [['a','b'],['c','d']];
+  $b = [];
+
+  foreach($a as $v1){
+    foreach($v1 as $v2){
+      $b[] = "$v2\n";
+    }
+  }
+  var_export($b);
+
+  $c = [
+    "one" => 1,
+    "two" => 2,
+    "three" =>3
+  ];
+  foreach($c as $k => $v){
+    echo "$k => $v.\n";
+  }
+
+  $arr = ['하나','둘','셋'];
+  reset($arr);
+  while(list(,$value) = each($arr)){
+    echo "$value<br>";
+  }
+  foreach($arr as $value){
+    echo "$value";
+  }
+
 ?>

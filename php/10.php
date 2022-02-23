c46f43f7e8ac436b851dfb0af6d230f45237dd9a
cats0713 <cats0713@naver.com>
2022-02-23 11:53:47 +0900

    php 변수 및 문자 출력

---
+++ php/10.php
@@ -0,0 +1,22 @@
+<?
+  echo "여러줄을 표현
+  합니다 산
+  토끼 토끼야
+  ";
+  echo "줄\n 바꿈";
+
+  echo '그냥 $num 표시하기';
+
+  echo 'this'.'is'.'apple';
+  echo 'this','is','apple';
+
+  $num = 'a';
+  
+  echo <<<END
+  이건 머야 $num 뭔지 모르겠네
+  라ㅣ넝라ㅓ"fdsdfsf"아러ㅣㅏㄴㅇㄹ
+  END;
+
+
+
+?>

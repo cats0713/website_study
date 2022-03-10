e294de6f7bee6d4d7cbc89d458238db55f1948e2
cats0713 <cats0713@naver.com>
2022-03-10 12:34:01 +0900

    파일업로드

--- php/fileDown.php
+++ php/fileDown.php
@@ -1,18 +1,19 @@
-<?
+<?php
   $filePath = './hello_world.txt';
   $fileSize = filesize($filePath);
   $pathParts = pathinfo($filePath);
   $fileName = $pathParts['basename'];
   $extension = $pathParts['extension'];
-  
-  header("pragma: public");
-  header("Expires: 0");
+
+  header("Pragma: public"); //이건뭐...
+  header("Expires: 0"); 
   header("Content-Type: application/octet-stream");
-  header("Content-Disposition: attachment; filename=$filename");
+  header("Content-Disposition: attachment; filename=$fileName");
   header("Content-Transfer-Encoding: binary");
   header("Content-Length: $fileSize");
 
-  ob_clean();
-  flush();
+  ob_clean(); //버퍼를(메모리) 깨끗하게 지우는것
+  flush(); //메모리에(버퍼에) 저장되어 있는 내용을 브라우저로 출력하고, 버퍼를 비운다는 뜻
   readfile($filePath);
+
 ?>
